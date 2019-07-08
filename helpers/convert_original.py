a7ab4ae0468d34b8fd5a595ff3a6b0465bb912d3
Taras Kucherenko <taras.svitozar@gmail.com>
2019-07-08 16:34:38 +0200

    Update data-processing

--- helpers/convert_original.py
+++ helpers/convert_original.py
@@ -8,79 +8,7 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], '../data_processing'))
 
 import numpy as np
-import pyquaternion as pyq
-from bvh_read.BVH import load
-from bvh_read.Animation import Animation
 from bvh_read.BVH_io import bvh2npy
-
-
-def rotation_to_position(frames, nodes):
-    """Convert bvh frames to body keypoint positions
-
-      Args:
-          frames:       bvh frames
-          nodes:        bvh hierarchy nodes
-
-      Returns:
-          out_data:     array containing body keypoint positions
-    """
-
-    output_lines = []
-
-    for frame in frames:
-        node_idx = 0
-        for i in range(51):
-            stepi = i * 3
-            z_deg = float(frame[stepi])
-            x_deg = float(frame[stepi + 1])
-            y_deg = float(frame[stepi + 2])
-
-            if nodes[node_idx]['name'] == 'End Site':
-                node_idx = node_idx + 1
-            nodes[node_idx]['rel_degs'] = [z_deg, x_deg, y_deg]
-            current_node = nodes[node_idx]
-
-            node_idx = node_idx + 1
-
-        for start_node in nodes:
-            abs_pos = np.array([0, 60, 0])
-            current_node = start_node
-            if start_node['children'] is not None:
-                for child_idx in start_node['children']:
-                    child_node = nodes[child_idx]
-
-                    child_offset = np.array(child_node['offset'])
-
-                    qz = pyq.Quaternion(axis=[0, 0, 1],
-                                        degrees=start_node['rel_degs'][0])
-                    qx = pyq.Quaternion(axis=[1, 0, 0],
-                                        degrees=start_node['rel_degs'][1])
-                    qy = pyq.Quaternion(axis=[0, 1, 0],
-                                        degrees=start_node['rel_degs'][2])
-                    qrot = qz * qx * qy
-                    offset_rotated = qrot.rotate(child_offset)
-                    child_node['rel_pos'] = start_node['abs_qt'].rotate(
-                        offset_rotated)
-
-                    child_node['abs_qt'] = start_node['abs_qt'] * qrot
-
-            while current_node['parent'] is not None:
-                abs_pos = abs_pos + current_node['rel_pos']
-                current_node = nodes[current_node['parent']]
-            start_node['abs_pos'] = abs_pos
-
-        line = []
-        for node in nodes:
-            line.append(node['abs_pos'])
-        output_lines.append(line)
-
-    output_array = np.asarray(output_lines)
-    out_data = np.empty([len(output_array), 192])
-    for idx, line in enumerate(output_array):
-        out_data[idx] = line.flatten()
-
-    return out_data
-
 
 def main():
     parser = argparse.ArgumentParser(
