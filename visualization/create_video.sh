42fc69cd87c4713c0269aae953e3ababadac5488
Taras Kucherenko <taras.svitozar@gmail.com>
2019-07-10 10:11:27 +0200

    Updated visualization script

--- visualization/create_video.sh
+++ visualization/create_video.sh
@@ -3,8 +3,9 @@
 # Set params
 start_time=120
 duration=10
-input_audio='data/audio_segment.wav'
-output_video='ground_truth_1.mp4'
+input_audio='data/audio_1.wav'
+input_gesture_file='../evaluation/data/predicted/MFCC/gesture1.txt'
+output_video='result_112_enc_1.mp4'
 
 end_time=$(( $start_time + $duration))
 
@@ -12,7 +13,7 @@ end_time=$(( $start_time + $duration))
 ffmpeg -ss $start_time -t $duration -i $input_audio audio_segment.wav
 
 # Generate video for gestures from the 3d position
-python model_animator.py --start=$start_time --end=$end_time --out='data/temp_gesture_video.mp4'
+python model_animator.py --start=$start_time --end=$end_time --input=$input_gesture_file --out='data/temp_gesture_video.mp4'
 
 # Add audio to the video
 ffmpeg -i 'data/temp_gesture_video.mp4' -i audio_segment.wav -c:v copy -map 0:v:0 -map 1:a:0 -c:a aac -b:a 192k gesture_w_audo.mp4
